c03d412046bad32b10e0b7dd104725f8f43343d7
kuift kotar <kuiftk@gmail.com>
2021-07-30 15:54:45 -0400

    implemented click mode, changed starting color

--- index.js
+++ index.js
@@ -29,8 +29,17 @@ function updateGrid(nbOfSquares) {
         function (event) {
             event.stopPropagation();
             if (event.target.className == "drawable") {
-                let colorValue = getPencilColor(event.target.style.background);
-                event.target.style.background = colorValue;
+                const clickMode = document.querySelector("#clickMode");
+                if (clickMode.checked) {
+                    if (leftMouseButtonOnlyDown) {
+                        let colorValue = getPencilColor(event.target.style.background);
+                        event.target.style.background = colorValue;
+                    }
+                }
+                else {
+                    let colorValue = getPencilColor(event.target.style.background);
+                    event.target.style.background = colorValue;
+                }
             }
         }
     );
@@ -46,21 +55,21 @@ function updateGrid(nbOfSquares) {
         newContainer.appendChild(div);
     }
 }
-let currentPenColor = "rgb(255,0,0)";
+let currentPenColor = "#80321E";
 function getPencilColor(currentCellColor) {
     const pentype = document.querySelector("#penType");
     const accumulation = document.querySelector("#accumulation");
     let newAccumulatedColor = "";
-    if(currentCellColor.length > 3 && accumulation.checked) //if the color accumulation button is checked, then we gradually increase the color of the current cell.
+    if (currentCellColor.length > 3 && accumulation.checked) //if the color accumulation button is checked, then we gradually increase the color of the current cell.
     {
         let currentRGBArray = currentCellColor.split("(")[1].split(")")[0].split(",");
         let accumulation = 0.05;
         let r = parseInt(currentRGBArray[0], 10);
-        r = Math.floor(Math.min(r * (1 + accumulation) + parseInt(currentPenColor.substring(1,3),16)*accumulation, 255));
+        r = Math.floor(Math.min(r * (1 + accumulation) + parseInt(currentPenColor.substring(1, 3), 16) * accumulation, 255));
         let g = parseInt(currentRGBArray[1], 10);
-        g = Math.floor(Math.min(g * (1 + accumulation) + parseInt(currentPenColor.substring(3,5),16)*accumulation, 255));
+        g = Math.floor(Math.min(g * (1 + accumulation) + parseInt(currentPenColor.substring(3, 5), 16) * accumulation, 255));
         let b = parseInt(currentRGBArray[2], 10);
-        b = Math.floor(Math.min(b * (1 + accumulation) + parseInt(currentPenColor.substring(5,7),16)*accumulation, 255));
+        b = Math.floor(Math.min(b * (1 + accumulation) + parseInt(currentPenColor.substring(5, 7), 16) * accumulation, 255));
         newAccumulatedColor = `rgb(${r}, ${g}, ${b})`;
         console.log(newAccumulatedColor);
     }
@@ -75,5 +84,16 @@ function getPencilColor(currentCellColor) {
 function rdmValue() {
     return Math.floor(Math.random() * 16777215).toString(16);
 }
+let leftMouseButtonOnlyDown = false;
+
+function setLeftButtonState(e) {
+  leftMouseButtonOnlyDown =
+    e.buttons === undefined ? e.which === 1 : e.buttons === 1;
+}
+
+document.body.onmousedown = setLeftButtonState;
+document.body.onmousemove = setLeftButtonState;
+document.body.onmouseup = setLeftButtonState;
+
 updateGrid(squaresNbSlider.value);
 
