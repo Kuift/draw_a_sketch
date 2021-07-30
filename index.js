7f9c7a4524d7cc25e9691687a82d074e8a9ee7ad
kuift kotar <kuiftk@gmail.com>
2021-07-30 14:17:09 -0400

    added custom pen

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 //create a div, give it the class of drawable, make it child to container, do this n x n time
 const squaresNbSlider = document.querySelector(".slider");
-squaresNbSlider.oninput = function(){
+squaresNbSlider.oninput = function () {
     const padDimensionTxt = document.querySelector("#padDimensionTxt");
     padDimensionTxt.innerText = `Pad dimension : ${squaresNbSlider.value} x ${squaresNbSlider.value}`;
     updateGrid(squaresNbSlider.value);
 }
 
 const clearButton = document.querySelector("#clearButton");
-clearButton.onclick = function(){
+clearButton.onclick = function () {
     updateGrid(squaresNbSlider.value);
 }
 
+const colorPicker = document.querySelector("#colorPicker");
+colorPicker.addEventListener("input", function(){
+    currentPenColor = this.value;
+});
 
-
-function updateGrid(nbOfSquares){
+function updateGrid(nbOfSquares) {
     const container = document.querySelector(".container");
     const everythingHolder = document.querySelector(".everythingHolder");
 
@@ -21,25 +24,31 @@ function updateGrid(nbOfSquares){
 
     const newContainer = document.createElement("div");
     newContainer.classList.add("container");
-    newContainer.setAttribute("style",`grid-template-columns: repeat(${nbOfSquares},auto);`);
+    newContainer.setAttribute("style", `grid-template-columns: repeat(${nbOfSquares},auto);`);
     newContainer.addEventListener("mouseover",
-      function (event) { 
-        event.stopPropagation();
-        if(event.target.className == "drawable"){
-            event.target.style.background = "red";}
+        function (event) {
+            event.stopPropagation();
+            if (event.target.className == "drawable") {
+                let colorValue = getPencilColor();
+                event.target.style.background = colorValue;
+            }
         }
     );
 
     everythingHolder.insertBefore(
-      newContainer,
-      document.querySelector(".sliderContainer")
+        newContainer,
+        document.querySelector(".sliderContainer")
     );
-    
+
     for (let i = 0; i < nbOfSquares * nbOfSquares; ++i) {
-      const div = document.createElement("div");
-      div.classList.add("drawable");
-      newContainer.appendChild(div);
+        const div = document.createElement("div");
+        div.classList.add("drawable");
+        newContainer.appendChild(div);
     }
+}
+let currentPenColor = "red";
+function getPencilColor() {
+    return currentPenColor;
 }
 updateGrid(squaresNbSlider.value);
 
